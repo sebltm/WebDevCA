42c0c77846e04342d8141c699c28834b26e4cac5
Sebastien Michel <sebltm@gmail.com>
2017-03-25 00:15:51 +0000

    25/03/2017  0:15:51.87

--- js/management.js
+++ js/management.js
@@ -1,14 +1,15 @@
 var currentGame = {
 	id: undefined,
+	sold: undefined,
 	title: undefined,
 	stock: undefined,
-	sold: undefined,
 	publisher: undefined
 };
 
-var req, sales, results, fetchData, emptyInfo, timepersale, amountpersale, propsensityToSellLow, propsensityToSellHigh, timepersale, amountpersale, gameid;
+var req, sales, results, fetchData, emptyInfo, timepersale, amountpersale, propsensityToSellLow, propsensityToSellHigh, timepersale, amountpersale, gameid, windowWidth;
 
 $(window).on("load", function () {
+	"use strict";
 	windowWidth = (typeof window.outerWidth !== 'undefined')?Math.max(window.outerWidth, $(window).width()):$(window).width();
     
     results(); //Fetch the games
@@ -61,21 +62,24 @@ $(window).on("load", function () {
 
 
 function showSearch() {
+	"use strict";
     setTimeout(function () {
         $("#gameinfo").hide();
-        $("#updatestock").hide();
         $("#removegame").hide();
+        $("#updatestock").hide();
 	}, 500); //Hide the game info with time for transitions
     
-    $("section").css({"margin": "0 0 0",
-    "height": "0vh"}); //Hide the section of game info, removes the useless scrollbar
+    $("section").css({
+		"margin": "0 0 0",
+    	"height": "0vh"
+	}); //Hide the section of game info, removes the useless scrollbar
     
     $("#game").css({
-        "border": "none",
 		"width": "60vw",
+        "border": "none",
 		"font-size": "1.8em",
-		"border-bottom": "2px solid rgba(0, 128, 0, 1)",
 		"border-radius": "0px",
+		"border-bottom": "2px solid rgba(0, 128, 0, 1)",
 		"box-shadow": "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
 	}); //Show the game search bar
 
@@ -84,43 +88,52 @@ function showSearch() {
 		this.style.boxShadow = "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)";
 	});
     
-    $("#gameresult").css({"border-bottom": "1px solid rgba(0, 0, 0, 1)", "height": "45vh"});
-	document.getElementById("game").setAttribute("placeholder", "Name of video game"); //Reset the placeholder
+    $("#gameresult").css({
+		"margin": "auto",
+		"height": "45vh",
+		"border-bottom": "1px solid rgba(0, 0, 0, 1)"
+	});
+	
+	$("#game").attr("placeholder", "Name of video game"); //Reset the placeholder
 }
 
 
 function fetchAndFormat(id) {
+	"use strict";
+	
     fetchData(id); //Fetch and display the data of the game
     
     if(windowWidth > 1020) {
-        $("section").css({"margin": "2% 0 0",
-    "height": "40vh"});  
+        $("section").css({
+			"margin": "2% 0 0",
+    		"height": "40vh"
+		});  
     } //For desktops, only allow the lower half of the screen
     
     else {
-        $("section").css({"margin": "2% 0 0",
-        "height": "100vh"});
+        $("section").css({
+			"margin": "2% 0 0",
+        	"height": "100vh"
+		});
     } //On mobiles, allow for more space
     
     /*Display all info sections with flexbox*/
-	$("#gameinfo").css({
-		"display": "flex"
-	}); 
-	
-	$("#updatestock").css({
-		"display": "flex"
-	});
+	$("#gameinfo").css({ "display": "flex" }); 
 
-	$("#removegame").css({
-		"display": "flex"
-	});
+	$("#removegame").css({ "display": "flex" });
+	
+	$("#updatestock").css({ "display": "flex" });
 
-    
-	document.getElementById("game").value = ""; //Empty the search bar of previous searches
+	$("#game").val(""); //Empty the search bar of previous searches
 	
-	$("#gameresult").css({ "max-height" : "0vh", "overflow-y": "hidden", "padding": "0%", "border-bottom": "1px solid rgba(0, 0, 0, 0)"}); //Hide the result box
+	$("#gameresult").css({
+		"padding": "0%",
+		"max-height" : "0vh",
+		"overflow-y": "hidden",
+		"border-bottom": "1px solid rgba(0, 0, 0, 0)"
+	}); //Hide the result box
 	
-	document.getElementById("game").setAttribute("placeholder", "Search"); //Put a smaller placeholder
+	$("#game").attr("placeholder", "Search"); //Put a smaller placeholder
 	
 	$("#game").css({
 		"width": "10vw",
@@ -142,6 +155,7 @@ function fetchAndFormat(id) {
 
 
 function fetchData(id) {
+	"use strict";
 		
 	if (sales) {
 		clearTimeout(sales); //Clear the sales simulator
@@ -157,24 +171,25 @@ function fetchData(id) {
         data = JSON.parse(data);
 		
 		currentGame.id = data.id;
+		currentGame.image = data.url;
+		currentGame.sold = data.sold;
 		currentGame.title = data.name;
 		currentGame.stock = data.stock;
-		currentGame.sold = data.sold;
-		currentGame.publisher = data.publisher;
-		currentGame.image = data.url;
         currentGame.price = data.price;
+		currentGame.publisher = data.publisher;
 		
-		document.getElementById("title").innerHTML = data.name;
-		document.getElementById("stock").innerHTML = "Stock: " + data.stock;
-		document.getElementById("sold").innerHTML = "Sold: " + data.sold;
-		document.getElementById("publisher").innerHTML = "Publisher: " + data.publisher;
-        document.getElementById("cost").innerHTML = "Price : £" + data.price;
+		
 		document.getElementById("update").value = data.stock;
+		document.getElementById("title").innerHTML = data.name;
+		document.getElementById("sold").innerHTML = "Sold: " + data.sold;
 		document.getElementById("gameimage").setAttribute("src", data.url);
+		document.getElementById("stock").innerHTML = "Stock: " + data.stock;
+        document.getElementById("cost").innerHTML = "Price : £" + data.price;
+		document.getElementById("publisher").innerHTML = "Publisher: " + data.publisher;
 		
 		document.getElementById("gameimage").onload = function() {
-			$("#bounceimage").hide();
 			$("#gameimage").show();
+			$("#bounceimage").hide();
 		};
 
         salesSimulator();
@@ -182,19 +197,25 @@ function fetchData(id) {
 }
 
 function salesSimulator() {
+	"use strict";
+	
     /*Sales simulator*/
     propsensityToSellLow = 23*currentGame.price; //Cheap games are getting bought at a faster
     propsensityToSellHigh = 55*currentGame.price;
-        
-    timepersale = Math.floor((Math.random() * propsensityToSellHigh) + propsensityToSellLow);
+    
     amountpersale = Math.floor((Math.random() * 10) + 1);
+    timepersale = Math.floor((Math.random() * propsensityToSellHigh) + propsensityToSellLow);
 
     sales = setTimeout(function () {
         if (currentGame.stock > 0) {
             currentGame.sold += amountpersale; //Update the game's new amount sold
             currentGame.stock -= amountpersale; //Update the game's new amout in stock
 
-            var stockupdate = $.post("/sm807/coursework/includes/stockupdate.php", {stock: currentGame.stock, sold: currentGame.sold, name: currentGame.title}); //Update the database
+            var stockupdate = $.post("/sm807/coursework/includes/stockupdate.php", {
+				stock: currentGame.stock,
+				sold: currentGame.sold,
+				name: currentGame.title
+			}); //Update the database
 
             stockupdate.done(function () {
                 fetchData(currentGame.id); //Fetch the latest information from the database
@@ -209,13 +230,19 @@ function results() {
 	"use strict";
     
 	var inputVal = $('#game').val(); //Get what the user has typed
-	$("#gameresult").css({"max-height" : "45vh", "overflow-y": "scroll", "padding": "1%"}); //Update the size of the result window
+	$("#gameresult").css({
+		"padding": "1%",
+		"max-height" : "45vh",
+		"overflow-y": "scroll"
+	}); //Update the size of the result window
 
 	if (inputVal.length) { //If the user has typed something, get results similar to the input
 		$.get("/sm807/coursework/includes/game_search.php", {term: inputVal}).done(function (data) {
 			$("#gameresult").empty();
 			if(data !== "<h2>No results</h2>") {
+				
 				data = JSON.parse(data);
+				
 				for(var i = 0; i<data.length; i++) {
 					$("#gameresult").append('<div gameid="'+data[i].id+'" class="gameresult"><h2>'+data[i].name+'</h2><img src="'+data[i].url+'"/></div>');
 				}
@@ -224,11 +251,8 @@ function results() {
 					gameid = parseInt(this.getAttribute("gameid"));
 					fetchAndFormat(gameid);
 				});
-			}
-			
-			else {
-				$("#gameresult").html(data);
-			}
+				
+			} else { $("#gameresult").html(data); }
             
 		});
 		
@@ -237,7 +261,9 @@ function results() {
 			
 			$("#gameresult").empty();
 			if(data !== "<h2>No results</h2>") {
+				
 				data = JSON.parse(data);
+				
 				for(var i = 0; i<data.length; i++) {
 					$("#gameresult").append('<div gameid="'+data[i].id+'" class="gameresult"><h2>'+data[i].name+'</h2><img src="'+data[i].url+'"/></div>');
 				}
@@ -246,12 +272,8 @@ function results() {
 					gameid = parseInt(this.getAttribute("gameid"));
 					fetchAndFormat(gameid);
 				});
-			}
-			
-			else {
-				$("#gameresult").html(data);
-			}
-			
+				
+			} else { $("#gameresult").html(data); }
 		});
 		
 	} else { $("#gameresult").html(""); } //Otherwise the window should be empty
@@ -263,17 +285,19 @@ function results() {
 
 
 function emptyInfo() { //Empty the different game info fields
+	"use strict";
+	
+	$("#cost").html("");
+	$("#sold").html("");
 	$("#title").html("");
 	$("#stock").html("");
-	$("#sold").html("");
 	$("#publisher").html("");
-	$("#cost").html("");
 	$("#gameimage").attr("src", "");
 	
 	currentGame.id = null; //Empty the current game object
+	currentGame.sold = null;
 	currentGame.title = null;
 	currentGame.stock = null;
-	currentGame.sold = null;
 	currentGame.publisher = null;
 	
 	$("#updatestock").hide();
