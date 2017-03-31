37b5ea549c84a5bf36fdb92ccfa91df8ba075e39
Sebastien Michel <sebltm@gmail.com>
2017-03-31 01:28:55 +0100

    31/03/2017  1:28:55.29

--- js/management.js
+++ js/management.js
@@ -371,12 +371,12 @@ function loadSale() {
 			.enter()
 			.append("rect")
 			.attr("x", function(d, i) {
-				return (i*(w/array.length));
+				return (i*(w/array.length)+margin);
 			})
 			.attr("y", function(d) {
 				return h - (d * h / max);
 			})
-			.attr("width", (w/array.length)/(margin*0.5/array.length))
+			.attr("width", (w/array.length)-(margin/array.length))
 			.attr("height", function(d) {
 				return yScale(0)-yScale(d);
 			})
